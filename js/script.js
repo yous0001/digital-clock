3dcd150c2ea8beaae8e14bbf1004cdcbdd93ca1e
Yousef emad mikhail <mad33833@gmail.com>
2024-08-20 20:04:13 +0300

    add date

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 let hours=document.getElementById("hours")
 let minutes=document.getElementById("minutes")
 let seconds=document.getElementById("seconds")
+let day=document.getElementById("day")
+let month=document.getElementById("month")
+let year=document.getElementById("year")
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 setInterval(()=>{
     let Time= new Date()
@@ -8,12 +12,10 @@ setInterval(()=>{
     hours.innerHTML=(Time.getHours()<10?"0":"")/*to put zero when time is one digit to make clock stay in 2 digits*/+Time.getHours()
     minutes.innerHTML=(Time.getMinutes()<10?"0":"")+Time.getMinutes()
     seconds.innerHTML=(Time.getSeconds()<10?"0":"")+Time.getSeconds()
-    
+    day.innerHTML=Time.getDay()
+    month.innerHTML= months[Time.getMonth()];
+    year.innerHTML=Time.getFullYear()
 },1000)
 
-let Time= new Date()
 
-hours.innerHTML=Time.getHours()
-minutes.innerHTML=Time.getMinutes()
-seconds.innerHTML=Time.getSeconds()
 
