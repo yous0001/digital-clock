7507a1a49311306a256e0eb8e68cbb7c6ad3948a
Yousef emad mikhail <mad33833@gmail.com>
2024-08-20 19:49:47 +0300

    fix one digit time issue

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ let seconds=document.getElementById("seconds")
 setInterval(()=>{
     let Time= new Date()
 
-    hours.innerHTML=Time.getHours()
-    minutes.innerHTML=Time.getMinutes()
-    seconds.innerHTML=Time.getSeconds()
+    hours.innerHTML=(Time.getHours()<10?"0":"")/*to put zero when time is one digit to make clock stay in 2 digits*/+Time.getHours()
+    minutes.innerHTML=(Time.getMinutes()<10?"0":"")+Time.getMinutes()
+    seconds.innerHTML=(Time.getSeconds()<10?"0":"")+Time.getSeconds()
     
 },1000)
 
