fb8d470fe980a8e075af0648aaab7403b73f9abf
Yousef emad mikhail <mad33833@gmail.com>
2024-08-20 20:19:33 +0300

    add 12 hours mode

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@ let day=document.getElementById("day")
 let month=document.getElementById("month")
 let year=document.getElementById("year")
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let mode=document.getElementById("mode")
 
 setInterval(()=>{
     let Time= new Date()
 
-    hours.innerHTML=(Time.getHours()<10?"0":"")/*to put zero when time is one digit to make clock stay in 2 digits*/+Time.getHours()
-    minutes.innerHTML=(Time.getMinutes()<10?"0":"")+Time.getMinutes()
+    hours.innerHTML=(Time.getHours()>12&&mode.checked)?(((Time.getHours()-12)<10?"0":"")+(Time.getHours()-12)):(Time.getHours()<10?"0":"")+Time.getHours();
+    minutes.innerHTML=(Time.getMinutes()<10?"0":"")/*to put zero when time is one digit to make clock stay in 2 digits*/+Time.getMinutes()
     seconds.innerHTML=(Time.getSeconds()<10?"0":"")+Time.getSeconds()
     day.innerHTML=Time.getDay()
     month.innerHTML= months[Time.getMonth()];
     year.innerHTML=Time.getFullYear()
 },1000)
+
+
+
 
 
 
